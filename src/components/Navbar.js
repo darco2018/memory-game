1d72054ef1bad6a65d7064c5a34add386fb8bd1a
darco2018 <newsecond@outlook.com>
2019-12-06 21:35:15 +0100

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Navbar.css';
 
-export default function Navbar(props) {
+//         (props.resetGame destructured)
+const Navbar = ({ resetGame }) => {
   return (
-    <div>
-      <button onClick={props.resetGame}>Reset</button>
-    </div>
+    <header>
+      <h2>
+        <a>Memory Game</a>
+      </h2>
+      <nav>
+        <li>
+          <a onClick={resetGame}>New Game</a>
+        </li>
+      </nav>
+    </header>
   );
-}
+};
 
 Navbar.propTypes = {
   resetGame: PropTypes.func.isRequired
 };
+
+export default Navbar;
